ab7bbb7af52cea507cbd677a4f7e2ff9dcc4dd8d
flybirdhub <2479438290@qq.com>
2019-02-20 21:22:02 +0800

    1，添加markdown

--- font-end/src/pages/editor/index.vue
+++ font-end/src/pages/editor/index.vue
@@ -1,9 +1,24 @@
 <template>
     <div class="editor">
-        <div id="test-editormd"></div>
+        <mavon-editor v-model="value" defaultOpen="preview" :subfield="false"/>
     </div>
 </template>
 
+<script type="text/ecmascript-6">
+    import { mavonEditor } from 'mavon-editor'
+    export default {
+        data () {
+            return {
+                value: '|column1|column2|column3|\n' +
+                    '|-|-|-|\n' +
+                    '|content1|content2|content3|'
+            }
+        },
+        components: {
+            mavonEditor
+        }
+    }
+</script>
 <style lang="less">
     @import 'index';
 </style>
